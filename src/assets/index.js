c97c0962435567ca5115988cc08d82ba97d2210b
ihsanuddienullah <ihsanuddienullah@gmail.com>
2020-12-09 16:58:41 +0700

    init

--- src/assets/index.js
+++
@@ -1,2 +0,0 @@
-export { default as imageExample } from './images/image_Example.jpg';
-export { default as image2 } from './images/image_Example2.jpg';
--- src/components/index.js
+++
@@ -1,2 +0,0 @@
-export { default as HeaderPage } from './headerPage';
-export { default as CoronaNews } from './coronaNews';
